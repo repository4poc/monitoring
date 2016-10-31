3fde77a4cee13e06ea9d4d6c480f917232ebe723
varindergupta82 <varinder_gupta82@yahoo.com>
2016-10-31 15:21:47 +0530

    Update FacebookDemo.java

--- src/FacebookDemo.java
+++ src/FacebookDemo.java
@@ -34,35 +34,6 @@ public class FacebookDemo {
 		Assert.assertEquals(result, verifyTxtMsg);
 	}
 
-	@Test(dependsOnMethods="testHomePage")
-	public void testLogin() throws Exception {
-		String verifyTxtMsg = "Varinder";
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.findElement(By.xpath("//*[@id='email']")).sendKeys(username);
-		driver.findElement(By.xpath("//*[@id='pass']")).sendKeys(password);
-		driver.findElement(By.xpath("/html/body/div/div[1]/div/div/div/div/div[2]/form/table/tbody/tr[2]/td[3]/label/input")).submit();
-		
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
-		String result = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div/div[1]/div/div/div/div[2]/div[1]/div[1]/div/a/span")).getText();
-		Assert.assertEquals(result, verifyTxtMsg);
-	}
-
-	private void extracted(String verifyTxtMsg, String result) {
-		Assert.assertEquals(result, verifyTxtMsg);
-	}
-	
-	@Test(dependsOnMethods="testLogin")
-	public void testLogOut() throws Exception {
-		String verifyTxtMsg = "How people build software";
-		driver.findElement(By.xpath("/html/body/div[1]/div[1]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/div/div[3]/div")).click();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.findElement(By.xpath("/html/body/div[23]/div/div/div/div/div[1]/div/div/ul/li[17]/a/span/span")).click();
-
-		System.out.println("Logout working...");
-		String result = driver.findElement(By.xpath("html/body/div[4]/div[1]/div/div/div[1]/h1")).getText();
-		Assert.assertEquals(result, verifyTxtMsg);
-	}
 
 	@AfterTest
 	public void tearDown() throws Exception {
