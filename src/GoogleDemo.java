cca8e5ff3d9073852c32cc19da8930a88f339328
varindergupta82 <varinder_gupta82@yahoo.com>
2016-11-08 12:08:51 +0530

    Update GoogleDemo.java

--- src/GoogleDemo.java
+++ src/GoogleDemo.java
@@ -2,7 +2,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
